a1de5fe9658a793a5bc4eea209393f27465d59eb
Tanvir Ahmed <61121499+TanvirAhmed1@users.noreply.github.com>
2025-05-17 00:31:52 +0600

    Database connection created

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using CSSMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSSMS.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Sale> Sales => Set<Sale>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Sale>()
+                .Property(s => s.TotalPrice)
+                .HasPrecision(18, 2);
+        }
+    }
+}
